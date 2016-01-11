27f4b205e77a8cc589d03976a6a46558914bd26c
Oskar Pearson <oskar@deckle.co.uk>
2016-01-11 13:00:52 +0000

    [s3_hosting] Added s3_sync

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'redcarpet'
 gem 'middleman-blog'
 gem 'middleman-blog-similar'
 gem 'builder'
+gem 'middleman-s3_sync'
 
 group :test do
   gem 'rspec'
--- config.rb
+++ config.rb
@@ -45,3 +45,23 @@ after_build do |builder|
   builder.source_paths << File.dirname(__FILE__)
   builder.copy_file('data/_redirects', 'build/_redirects')
 end
+
+activate :s3_sync do |s3_sync|
+  # s3_sync.bucket                     = # ENV['AWS_BUCKET']
+  # s3_sync.aws_access_key_id          = # ENV['AWS_ACCESS_KEY_ID']
+  # s3_sync.aws_secret_access_key      = # ENV['AWS_SECRET_ACCESS_KEY']
+  s3_sync.region                     = 'eu-west-1'     # The AWS region for your bucket.
+  s3_sync.delete                     = true # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default.
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.version_bucket             = false
+  s3_sync.index_document             = 'index.html'
+  s3_sync.error_document             = '404.html'
+end
+
+# Without this we trigger a bug in s3_sync:
+::Rack::Mime::MIME_TYPES['.woff2'] = 'font/woff2'
